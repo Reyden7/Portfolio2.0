427de7828821acf4d876a91f641c489d1c281234
Quentin <quentin.cordiero@neuf.fr>
2026-06-23 17:51:24 +0200

    Remove page loading transitions

--- qc-tech-portfolio/src/app/providers.jsx
+++ qc-tech-portfolio/src/app/providers.jsx
@@ -1,26 +1,16 @@
 "use client";
 
-import { useState } from "react";
-import Loader from "../components/Loader/Loader";
 import CustomCursor from "../components/CustomCursor/CustomCursor";
 import ScrollProgress from "../components/ScrollProgress/ScrollProgress";
 import { PageTransitionProvider } from "../context/PageTransitionContext";
 
 export default function Providers({ children }) {
-  const [isLoading, setIsLoading] = useState(true);
-
   return (
     <>
       <CustomCursor />
       <ScrollProgress />
 
-      {isLoading && <Loader onComplete={() => setIsLoading(false)} />}
-
-      {!isLoading && (
-        <PageTransitionProvider>
-          {children}
-        </PageTransitionProvider>
-      )}
+      <PageTransitionProvider>{children}</PageTransitionProvider>
     </>
   );
 }
